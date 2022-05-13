ad8b9b9a3b2dc55f6520ed5c9fdc6cb2d2f1f97f
M4TH3US17 <matheusdalvino50@gmail.com>
2022-05-13 17:01:53 -0400

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entitites/pk/ScorePK.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dsmovie.entitites.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.devsuperior.dsmovie.entitites.Movie;
+import com.devsuperior.dsmovie.entitites.User;
+
+@Embeddable
+public class ScorePK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ScorePK() {
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
